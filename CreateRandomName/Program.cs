3cc646fc3ee7625c24f70c9a1143f233f4d6b6f6
code diamond <KingJamin@hotmail.com>
2019-11-11 19:41:09 -0600

    Initial commit

---
+++ CreateRandomName/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace CreateRandomName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int number = random.Next(0, 21986);
+            int number2 = random.Next(0, 21986);
+
+            string firstName = File.ReadLines("names.txt").Skip(number-1).Take(1).First();
+            string lastName = File.ReadLines("names.txt").Skip(number2 - 1).Take(1).First();
+            string line = firstName + " " + lastName;
+            Console.WriteLine(line);
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            System.Console.ReadKey();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# CreateRandomName
+ C# Generates random first and last name
